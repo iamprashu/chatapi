37b7640d8e1a4c65e2b21145aabadc5500bf9d43
iamprashu <iamprashu.gs@gmail.com>
2025-11-23 22:32:46 +0530

    Added cookie parser middleware

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/user/userRoute";
 import DbConnect from "./libs/db";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app: Express = express();
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 
